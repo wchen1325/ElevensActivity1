a617d12304a79b4503a3cb467790b989f8161b08
BT_1N3_15 <wchen1325@bths.edu>
2019-02-14 14:42:16 -0500

    ok

--- src/CardTester.java
+++ src/CardTester.java
@@ -2,4 +2,16 @@
  * Created by Teacher on 1/7/2019.
  */
 public class CardTester {
+    public static void main(String args[]) {
+        Card meow = new Card("M", "Neko", 100);
+        Card meow2 = new Card("M", "Neko", 100);
+        Card Woof = new Card("W", "Inu", 99);
+
+        System.out.println(meow.matches(meow2));
+        System.out.println(meow.matches(Woof));
+        System.out.println(Woof.matches(meow2));
+
+        System.out.println(meow.toString());
+
+    }
 }
